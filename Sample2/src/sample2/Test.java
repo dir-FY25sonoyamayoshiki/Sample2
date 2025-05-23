f7a0676719cf48420197312e689f4c816c3bdc04
dir-FY25sonoyamayoshiki <yoshiki.sonoyama.q252@dirbato.co.jp>
2025-05-23 14:58:01 +0900

    2025-05-23_Lesson2_Ch2

---
+++ Sample2/src/sample2/Test.java
@@ -0,0 +1,14 @@
+package sample2;
+
+public class Test {
+    //mainメソッドはプログラムの最初に動作します。
+    public static void main(String[] args) {
+        /*コメントアウト
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        */
+        System.out.println("学習を始めましょう！");
+
+    }
+
+}
